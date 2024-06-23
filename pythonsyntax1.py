274ca890e29fb6b3f7be379ca3581a3c4b5b2794
DanPaul12 <dan.paul.schechter@gmail.com>
2024-06-22 18:07:12 -0700

    python syntax practice 1

---
+++ pythonsyntax1.py
@@ -0,0 +1,33 @@
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+feeling = input()
+if feeling == "sad":
+    print("Hope ur day gets better!")
+if feeling == "happy":          
+    print("Gr8 to hear!")
+
+
+PI_VALUE = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+
+turkey = 4.99
+bread = 3.50
+tomatoes = 2.89
+print(turkey + bread + tomatoes)
+
+
+amt = 10000
+int = 7
+years = 1
+future_value = amt * ((1 + (0.01 * int)) ** years)
+print(round(future_value, 2))
+
+
