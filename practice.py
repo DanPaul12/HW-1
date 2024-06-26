0d9523a5663b32fe038a50d316a1b7c48a81ec9c
DanPaul12 <dan.paul.schechter@gmail.com>
2024-06-25 22:41:47 -0700

    hw

---
+++ practice.py
@@ -0,0 +1,9 @@
+personality = input("Are u scare or no?: ")
+taste = input("Do you have good taste?: ")
+price = input("are u cheep?: ")
+if personality == "yes" and taste == "yes":
+    print("get a lil cup")
+if personality == "yes" and taste == "no":
+    print("get a lil cup w cream")
+if price == "yes" and taste == "no":
+    print("get a lil cup w cream")
